4e777d888d38576a9ae5dce12d4596f6e2c1753a
henrique silva <henrique@gmail.com>
2024-03-24 14:26:22 -0300

    dedo

--- js.api/Apiaddtabela.js
+++ js.api/Apiaddtabela.js
@@ -1,16 +1,11 @@
  export class Apiaddtabela{
     async getApiaddtabela(){
-<<<<<<< HEAD
-        let Tabela = await fetch ("http://127..0.0.1:8080/projeto/<projeto_input>")
-=======
         let Tabela = await fetch ("http://127.0.0.1:6969/projeto/<projeto_input>")
->>>>>>> 970a553be043aee4bc7d7bcdb65e27d31943c0fe
         method: "POST"
         .then( (response) => {
             return response.json()
         })
 
         return Tabela; 
-
-}
+   }
 }
