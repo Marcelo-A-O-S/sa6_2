970a553be043aee4bc7d7bcdb65e27d31943c0fe
192el <samuelmagoncalves@gmail.com>
2024-03-24 14:08:40 -0300

    htpp tava errado

--- js.api/Apiaddgrupo.js
+++ js.api/Apiaddgrupo.js
@@ -1,6 +1,6 @@
 export class Apiaddgrupos {
     async getApiaddgrupos() {
-        let Grupo = await fetch("htpp://127..0.0.1:6969/grupo/<grupo_input>", {
+        let Grupo = await fetch("http://127.0.0.1:6969/grupo/<grupo_input>", {
             method: "POST"
         })
             .then((response) => {
--- js.api/Apigetgrupo.js
+++ js.api/Apigetgrupo.js
@@ -1,6 +1,6 @@
 export class Apigetgrupo{
     async getApigetgrupo(){
-        let getGrupo = await fetch ("htpp://127..0.0.1:6969/grupo/<nome>/<senha>")
+        let getGrupo = await fetch ("http://127.0.0.1:6969/grupo/<nome>/<senha>")
         method: "GET"
         .then( (response) => {
             return response.json()
